2cbfee84daebea27c0afb185a3c6bf65d2b1b409
CesareDev <corsicesare.lavoro@gmail.com>
2023-12-08 13:58:23 +0100

    Modified lua file

--- premake5.lua
+++ premake5.lua
@@ -19,29 +19,76 @@ project "SFML-Fractal"
 
     if _ACTION == "vs2022" then
         libdirs { "Dependencies/lib/vc" }
-        links { "winmm", "vorbisenc", "vorbisfile", "vorbis", "ogg", "flac", "gdi32", "freetype", "opengl32", "sfml-main" }
+        links { 
+            "winmm", 
+            "vorbisenc", 
+            "vorbisfile", 
+            "vorbis", 
+            "ogg", 
+            "flac", 
+            "gdi32", 
+            "freetype", 
+            "opengl32", 
+            "sfml-main"
+        }
     end
+    
     if _ACTION == "gmake2" then
         libdirs { "Dependencies/lib/mingw" }
-        links { "winmm:static", "vorbisenc:static", "vorbisfile:static", "vorbis:static", "ogg:static", "flac:static", "gdi32:static", "freetype:static", "opengl32:static", "sfml-main:static" }
+        links { 
+            "winmm:static", 
+            "vorbisenc:static", 
+            "vorbisfile:static", 
+            "vorbis:static", 
+            "ogg:static", 
+            "flac:static", 
+            "gdi32:static", 
+            "freetype:static", 
+            "opengl32:static", 
+            "sfml-main:static" 
+        }
     end
 
     filter "configurations:Debug"
         if _ACTION == "vs2022" then
-            links { "sfml-system-s-d", "sfml-window-s-d", "sfml-graphics-s-d", "sfml-audio-s-d","sfml-network-s-d" }
+            links { 
+                "sfml-graphics-s-d",
+                "sfml-window-s-d",
+                "sfml-system-s-d", 
+                "sfml-audio-s-d",
+                "sfml-network-s-d" 
+            }
         end
         if _ACTION == "gmake2" then
-            links { "sfml-system-s-d:static", "sfml-window-s-d:static", "sfml-graphics-s-d:static", "sfml-audio-s-d:static", "sfml-network-s-d:static" }
+            links { 
+                "sfml-graphics-s-d:static",
+                "sfml-window-s-d:static", 
+                "sfml-system-s-d:static", 
+                "sfml-audio-s-d:static", 
+                "sfml-network-s-d:static" 
+            }
         end
         defines { "DEBUG" }
         symbols "On"
 
     filter "configurations:Release"
         if _ACTION == "vs2022" then
-            links { "sfml-system-s", "sfml-window-s", "sfml-graphics-s", "sfml-audio-s", "sfml-network-s" }
+            links { 
+                "sfml-graphics-s", 
+                "sfml-window-s", 
+                "sfml-system-s", 
+                "sfml-audio-s", 
+                "sfml-network-s" 
+            }
         end
         if _ACTION == "gmake2" then
-            links { "sfml-system-s:static", "sfml-window-s:static", "sfml-graphics-s:static", "sfml-audio-s:static", "sfml-network-s:static" }
+            links { 
+                "sfml-graphics-s:static", 
+                "sfml-window-s:static", 
+                "sfml-system-s:static", 
+                "sfml-audio-s:static", 
+                "sfml-network-s:static" 
+            }
         end
 
         defines { "NDEBUG" }
